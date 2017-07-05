1a2c3ce927945fa9b472f9850146d033760dcec5
mikezander <mikezander87@yahoo.com>
2017-07-05 15:56:47 -0400

    initial commit

---
+++ SkateSpots/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SkateSpots
+//
+//  Created by Michael Alexander on 7/5/17.
+//  Copyright © 2017 Michael Alexander. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
