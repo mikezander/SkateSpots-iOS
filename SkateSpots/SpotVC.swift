9e49ec9919e0ec79925f1db8196aa6705f8127f1
mikezander <mikezander87@yahoo.com>
2017-07-08 13:21:15 -0400

    added SpotVC and implemented image picker

---
+++ SkateSpots/SpotVC.swift
@@ -0,0 +1,52 @@
+//
+//  SpotVC.swift
+//  SkateSpots
+//
+//  Created by Michael Alexander on 7/8/17.
+//  Copyright © 2017 Michael Alexander. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import Photos
+
+class SpotVC:UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
+    var imagePicker: UIImagePickerController!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imagePicker = UIImagePickerController()
+        imagePicker.allowsEditing = true
+        imagePicker.delegate = self
+        
+        showPhotoActionSheet()
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
+    func showPhotoActionSheet(){
+        let actionSheet = UIAlertController(title: "Photo Source", message: "Choose a source", preferredStyle: .actionSheet)
+        
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (action:UIAlertAction) in
+            if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                self.imagePicker.sourceType = .camera
+                self.present(self.imagePicker, animated: true, completion: nil)
+            } else { print("Sorry cant take photo") }
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: { (action:UIAlertAction) in
+            self.imagePicker.sourceType = .photoLibrary
+            self.present(self.imagePicker, animated: true, completion: nil)
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler:nil))
+        
+        present(actionSheet, animated: true, completion: nil)
+    }
+
+}
