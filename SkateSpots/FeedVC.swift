7158ab15edc8c0f75c4ce51caa0c770d8ac5e5c4
mikezander <mikezander87@yahoo.com>
2017-07-08 12:39:33 -0400

    configured tab bar controller

--- SkateSpots/FeedVC.swift
+++ SkateSpots/FeedVC.swift
@@ -10,7 +10,7 @@ import UIKit
 import Firebase
 import FirebaseAuth
 
-class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource{
+class FeedVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource{
 
     @IBOutlet weak var spotCollectionView: UICollectionView!
     
