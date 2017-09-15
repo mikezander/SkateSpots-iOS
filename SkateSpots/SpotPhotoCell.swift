75788a32fea968c6ae8f2a53a5858491e7caae9d
mikezander <mikezander87@yahoo.com>
2017-09-14 22:08:06 -0400

    refactor

--- SkateSpots/SpotPhotoCell.swift
+++ SkateSpots/SpotPhotoCell.swift
@@ -58,7 +58,6 @@ class SpotPhotoCell: UICollectionViewCell{
                             
                         }
                         
-                        
                     }
                 }
                 
