9ee68f5b24a54541d3248673318bfa6b147420df
Michael Alexander <mikezander0912@gmail.com>
2019-10-05 11:10:27 -0400

    Update MapCollectionViewCell.swift

--- SkateSpots/MapCollectionViewCell.swift
+++ SkateSpots/MapCollectionViewCell.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Firebase
+
 
 class MapCollectionViewCell: UICollectionViewCell {
     
@@ -15,6 +17,8 @@ class MapCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var spotType: UILabel!
     @IBOutlet weak var borderedView: UIView!
     @IBOutlet weak var spotCountLabel: UILabel!
+    @IBOutlet weak var ratingImageView: UIImageView!
+    @IBOutlet weak var ratingLabel: UILabel!
     
     func configureCell(spot: Spot, style: String) {
         
@@ -32,11 +36,37 @@ class MapCollectionViewCell: UICollectionViewCell {
             spotName.textColor = .white
             spotType.textColor = .white
             spotCountLabel.textColor = .white
+            ratingLabel.textColor = .white
         } else {
             borderedView.backgroundColor = .white
             spotName.textColor = .black
             spotType.textColor = .black
             spotCountLabel.textColor = .black
+            ratingLabel.textColor = .black
         }
+        
+        let refCurrentSpot = DataService.instance.REF_SPOTS.child(spot.spotKey)
+        
+        refCurrentSpot.observeSingleEvent(of: .value, with: { (snapshot) in
+            if let ratingTally = snapshot.childSnapshot(forPath: "rating").value as? Double{
+                let ratingVotes = snapshot.childSnapshot(forPath: "ratingVotes").value as! Int
+                
+                var rating = ratingTally / Double(ratingVotes)
+                rating = (rating * 10).rounded() / 10
+                DispatchQueue.main.async {
+                    self.ratingLabel.text = "\(rating)"
+                    self.ratingImageView.alpha = 1.0
+                    self.ratingLabel.alpha = 1.0
+                }
+                
+            }else{
+                DispatchQueue.main.async {
+                    self.ratingImageView.alpha = 0.0
+                    self.ratingLabel.alpha = 0.0
+                }
+                
+            }
+            
+        })
     }
 }
