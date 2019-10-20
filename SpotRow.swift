3c182f66507d9d0f29aac3b8b09453fb56b52f9d
Michael Alexander <mikezander0912@gmail.com>
2019-10-20 18:10:06 -0400

    Added labels to SpotRow

--- SpotRow.swift
+++ SpotRow.swift
@@ -9,9 +9,11 @@
 import UIKit
 import FirebaseDatabase
 import FirebaseStorage
+import AVFoundation
 
 protocol SpotRowDelegate{
     func didTapDirectionsButton(spot: Spot)
+    func getCellHeight(height: CGFloat)
 }
 
 class SpotRow: UITableViewCell {
@@ -26,6 +28,9 @@ class SpotRow: UITableViewCell {
     @IBOutlet weak var pageControl: UIPageControl!
     @IBOutlet weak var pageLabel: UILabel!
     @IBOutlet weak var pageContainer: UIView!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var distanceContainer: UIView!
+    @IBOutlet weak var distanceLabel: UILabel!
     
     var delegate: SpotRowDelegate?
     
@@ -47,18 +52,48 @@ class SpotRow: UITableViewCell {
         self.spotName.adjustsFontSizeToFitWidth = true
         self.spotLocation.text = spot.spotLocation
         
+        var spotLocationString = ""
+        
+        if spot.spotLocation.contains("United States") {
+            if let spotCityState = spot.spotLocation.components(separatedBy: "-").first {
+                let comp = spotCityState.components(separatedBy: ",")
+                if comp.count == 2 {
+                    spotLocationString = "\(comp[0]), \(comp[1])"
+                } else {
+                    spotLocationString = spotCityState
+                }
+            } else {
+                spotLocationString = spot.spotLocation
+            }
+        } else {
+            let comp = spot.spotLocation.components(separatedBy: ",")
+            if comp.count == 2 {
+                spotLocationString = "\(comp[0]), \(comp[1])"
+            } else {
+                spotLocationString = spot.spotLocation
+            }
+            
+            //spotLocationString = spot.spotLocation
+        }
+
+        locationLabel.text = spotLocationString
+        
         userImage.isUserInteractionEnabled = true
         
         self.userImage.sd_setImage(with: URL(string: spot.userImageURL), placeholderImage: UIImage(named: "profile-placeholder"))
        
-        if spot.distance != nil{
+        if spot.distance != nil {
+            distanceContainer.isHidden = false
+
             spotDistance.isHidden = false
             miLabel.isHidden = false
             
             let distanceToSpot = String(format: "%.1f", spot.distance!)
-            self.spotDistance.text = distanceToSpot
+            //self.spotDistance.text = distanceToSpot
+            self.distanceLabel.text = distanceToSpot
             
         } else{
+            distanceContainer.isHidden = true
             spotDistance.isHidden = true
             miLabel.isHidden = true
         }
@@ -92,17 +127,7 @@ extension SpotRow : UICollectionViewDataSource {
         pageLabel.text = "\(page) / \(spot.imageUrls.count)"
         
     }
-    
-//    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-//        func scrollViewDidScroll(_ scrollView: UIScrollView) {
-//            let witdh = scrollView.frame.width - (scrollView.contentInset.left*2)
-//            let index = scrollView.contentOffset.x / witdh
-//            let roundedIndex = ceil(index)
-//            pageControl?.currentPage = Int(roundedIndex)
-//            pageLabel.text = "\(Int(roundedIndex)) / \(spot.imageUrls.count)"
-//        }
-//    }
-    
+
     override func prepareForReuse() {
         super.prepareForReuse()
 
@@ -115,16 +140,6 @@ extension SpotRow : UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
         
-
-//
-//        pageControl.currentPage = indexPath.row
-//        pageLabel.text = "\(indexPath.row + 1) / \(imageCount)"
-        
-        
-//        pageControl.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
-//        pageControl.hidesForSinglePage = true
-//        pageControl.currentPage = indexPath.row
-//        pageLabel.text = "\(indexPath.row + 1) / \(spot.imageUrls.count)"
     }
     
     func collectionView(_ collectionView: UICollectionView, didEndDisplaying cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
@@ -149,16 +164,14 @@ extension SpotRow : UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         collectionView.setContentOffset(CGPoint.zero, animated: false)
-        
-       // return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
-
         let screenSize = UIScreen.main.bounds
         let screenHeight = screenSize.height
         let screenWidth = screenSize.width
 
-        let heightOffset:CGFloat = 225
+        let heightOffset: CGFloat = 225
         let contentHeight = screenHeight - heightOffset
 
+        delegate?.getCellHeight(height: contentHeight)
         return CGSize(width: screenWidth, height: contentHeight)
     }
     
